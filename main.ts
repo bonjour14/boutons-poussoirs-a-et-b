41703a2de57edc62f30d0548df1040985d395b26
bonjour14 <103410186+bonjour14@users.noreply.github.com>
2022-05-09 09:10:33 +0200

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,26 @@
+basic.showIcon(IconNames.Heart)
+basic.forever(function () {
+    if (input.buttonIsPressed(Button.A)) {
+        basic.showLeds(`
+            . # # . .
+            # . . # .
+            # # # # .
+            # . . # .
+            # . . # .
+            `)
+    }
+    if (input.buttonIsPressed(Button.B)) {
+        basic.showLeds(`
+            # # # . .
+            # . . # .
+            # # # . .
+            # . . # .
+            # # # . .
+            `)
+    }
+})
+basic.forever(function () {
+    if (input.buttonIsPressed(Button.AB)) {
+        basic.clearScreen()
+    }
+})
